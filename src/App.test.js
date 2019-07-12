391c8334ee825298a6485eab27e625965e7a7178
titus efferian <titus.efferian@tokopedia.com>
2019-07-12 10:38:59 +0700

    smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Header from './Components/HeaderSearch';
+import Content from './Components/Content';
+import { SEARCH_ENDPOINT } from './apis';
 
-it('renders without crashing', () => {
+it('renders content without crashing with no data', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Content data={[]}/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders content without crashing with data', async () => {
+  const div = document.createElement('div');
+  const result = await fetch(`${SEARCH_ENDPOINT}miku`)
+  const parse = await result.json()
+  ReactDOM.render(<Content data={parse.data}/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders headers without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header value='' onClick={()=>{}} onChange={()=>{}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
