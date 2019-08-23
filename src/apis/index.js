8cce7d76d2aa1ff6da623bac55876f1cbd6c5f5d
titus efferian <titus.efferian@tokopedia.com>
2019-08-23 19:04:42 +0700

    https'

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,6 @@
 export const giphyKey = 'xtQm4zLq33bocPsDatGDr7uBwvz9VFQN'
 
 export const limit = 20
-export const SEARCH_ENDPOINT = `http://api.giphy.com/v1/gifs/search?limit=${limit}&api_key=${giphyKey}&q=`
+export const SEARCH_ENDPOINT = `https://api.giphy.com/v1/gifs/search?limit=${limit}&api_key=${giphyKey}&q=`
 export const RANDOM_ENDPOINT = `https://api.giphy.com/v1/gifs/random?api_key=${giphyKey}`
 export const TRENDING_ENDPOINT = `https://api.giphy.com/v1/gifs/trending?api_key=${giphyKey}&limit=${limit}`
